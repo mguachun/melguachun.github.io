4bfb293dc15d7465c2d434d697175460d86f5190
Melissa Guachun <1212malka@gmail.com>
2022-02-23 17:54:46 -0500

    changed navigation

---
+++ src/Navigation.js
@@ -0,0 +1,33 @@
+import React from "react";
+// import { Link } from "react-scroll";
+import {Nav} from 'react-bootstrap'
+function Navigation() {
+  return (
+    <div id="nav">
+     
+      <Nav defaultActiveKey="/home" as="ul">
+      <div className="nav-left">
+        <p className="nav-name">Melissa Guachun
+          <img src={require("./images/computer-plain.jpg")} alt="logo" id="logo" />
+        </p>
+      </div>
+
+        <Nav.Item as="li" className="nav-right">
+          <Nav.Link href="/#about">About</Nav.Link>
+        </Nav.Item>
+        <Nav.Item as="li">
+          <Nav.Link href="/#skills" className="nav-right">Skills</Nav.Link>
+        </Nav.Item>
+        <Nav.Item as="li">
+          <Nav.Link href="/#projects" className="nav-right">Projects</Nav.Link>
+        </Nav.Item>
+        <Nav.Item as="li">
+          <Nav.Link href="/#contact" className="nav-right">Contact</Nav.Link>
+        </Nav.Item>
+      </Nav>
+
+    </div>
+
+  )
+}
+export default Navigation;

2542a26c18432273b8f33487459f55a3fdb3b2f6
Melissa Guachun <1212malka@gmail.com>
2022-02-23 17:54:18 -0500

    wrapped everything in divs

--- src/Projects.js
+++ src/Projects.js
@@ -1,7 +1,10 @@
 import React from "react";
 function Projects() {
     return(
+        <div id="projects">
         <h1>Project Page</h1>
+        </div>
+      
     )
 }
 export default Projects;

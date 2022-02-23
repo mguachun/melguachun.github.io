e35de11c8cf33d98524a1fd95890eb3aac638453
Melissa Guachun <1212malka@gmail.com>
2022-02-23 16:34:23 -0500

    setting navbar pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React from 'react';
-import { Link } from "react-scroll";
 import About from "./About";
 import Skills from "./Skills";
 import Contact from "./Contact";
-import Projects from "./Projects"
+import Projects from "./Projects";
 import Navbar from "./Navbar";
-import './App.css';
 
+import './App.css';
 
 function App() {
   return (
     <div>
       <Navbar />
-      <h1>Hello</h1>
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+
+
+  
     </div>
 
-  )
+  );
 }
 
 export default App;

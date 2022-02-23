6401745b4364c3cd8fb20b9ce9a85d1d079e4e87
Melissa Guachun <1212malka@gmail.com>
2022-02-23 16:54:30 -0500

    start bulding home

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,25 @@
 import React from "react";
+import computer from "./images/computer-plain.jpg"
+
 function Home() {
     return(
-        <h1>Home Page</h1>
+        <div id="home" className="content">
+        <div className="container">
+          <div className="row justify-content-center home-container">
+            <div className="col-md-5">
+              <img className="profile-image" ></img>
+              {/* src={pic} */}
+            </div>
+            <div className="col-md-5 justify-content-center">
+              <div className="home-name">
+                Hey, I'm Melissa <span><img  className="home-smiley"/></span>
+                {/* src={smile} */}
+              </div>
+            
+            </div>
+          </div>
+        </div>
+      </div>
     )
 }
 export default Home;

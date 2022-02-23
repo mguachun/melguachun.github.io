a36f474a22b4a0d38b237ae24a6a64fd62644ce5
Melissa Guachun <1212malka@gmail.com>
2022-02-23 17:54:11 -0500

    wrapped everything in divs

--- src/Contact.js
+++ src/Contact.js
@@ -1,7 +1,10 @@
 import React from "react";
 function Contact() {
     return(
+        <div id="contact">
         <h1>Contact Page Here</h1>
+        </div>
+      
     )
 }
 export default Contact;

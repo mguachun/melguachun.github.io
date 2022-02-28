a15bcd170c5ec5910c7eaefa02c155fbc581a5a5
Melissa Guachun <1212malka@gmail.com>
2022-02-28 13:27:49 -0500

    got rid of unneeded div

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,21 @@
 import React from "react";
 
-
-
 function Home() {
     return(
-        <div id="home" className="content">
+ 
         <div className="container">
           <div className="row justify-content-center home-container">
             <div className="col-md-5">
               <img className="profile-image"  src={require("./images/IMG_3680.jpg")} alt="profile-img" id="profile-img"></img>
               <div id="intro">
-            <h1 > Hey, I'm Melissa </h1>
+                <br></br>
+                <h1 > Hey, I'm Melissa </h1>
                 <p>Software Engineer, Visual Artist, Creative </p> 
             </div>
             </div>
           </div>
         </div>
-      </div>
+
     )
 }
 export default Home;

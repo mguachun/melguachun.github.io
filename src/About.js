9f827bb4504d66f346f89c0d2af12284587d9ac7
Melissa Guachun <1212malka@gmail.com>
2022-02-28 13:45:58 -0500

    added to blurb

--- src/About.js
+++ src/About.js
@@ -8,7 +8,11 @@ function About() {
                 I've always had an interest in image and text through my art. Through coding, I'm able to converge
                 my passions for language and design to positively impact others. As a sortware engineer, 
                 I strive to be the connection between the technical world of coding and the visual aesthetics of fine 
-                art to create functional yet visually stunning projects.
+                art to create functional yet visually stunning projects. I'm a graduate of the Flatiron School where I 
+                spent 10 months refining my skills as a full stack developer. I am eager to find my place on a 
+                collaborative team of talented individuals where I can continue expanding my knowledge and developing new skills.
+                When I'm not coding new projects, you can find me working on my art, digging through crates at a record store, or 
+                exploring my neighborhood with my dog Fletcher.
            </h4>
            <img src={require("./images/computers.jpg")} id="computer" alt="computer illustration with colors coming out of the monitor"/>
 

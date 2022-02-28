f5482c881a6d490d4a3f989a3cf100a4af703f50
Melissa Guachun <1212malka@gmail.com>
2022-02-28 12:35:55 -0500

    deleted unneeded import

--- src/About.js
+++ src/About.js
@@ -1,10 +1,11 @@
 import React from "react";
-import './App.css';
+
 function About() {
     return(
         <div>
             <h4>
-            <img src={require("./images/computers.jpg")} id="computer"/>
+                I'm a software engineer with a background in visual arts and design. 
+            <img src={require("./images/computers.jpg")} id="computer" alt="computer illustration with colors coming out of the monitor"/>
            </h4>
 
         </div>

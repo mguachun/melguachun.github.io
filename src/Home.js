a3171df358fa12c101d968c9380fde2c90ecd0a3
Melissa Guachun <1212malka@gmail.com>
2022-03-16 11:53:08 -0400

    imported selfie

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import self from "./images/profile_img.png"
 
 function Home() {
     return(
@@ -6,8 +7,9 @@ function Home() {
         <div className="container">
           <div className="row justify-content-center home-container">
             <div className="col-md-5">
-              <img className="profile-image"  src={require("./images/IMG_3680.jpg")} alt="profile-img" id="profile-img"></img>
+              <img className="profile-image"  src={self} alt="profile-img" id="profile-img"></img>
               <div id="intro">
+              {/* src={require("./images/profile_img.png")} */}
                 <br></br>
                 <h1 > Hey, I'm Melissa </h1>
                 <p>Software Engineer, Visual Artist, Creative </p> 

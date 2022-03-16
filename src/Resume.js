8b1ec30ac38af41679813eb338a544c8506e67c9
Melissa Guachun <1212malka@gmail.com>
2022-03-16 17:09:04 -0400

    resume  component built out

---
+++ src/Resume.js
@@ -0,0 +1,10 @@
+import React from "react";
+import CV from "./images/MelissaGuachun_22.png";
+function Resume() {
+    return(
+            <div>
+                <img className="resume" src={CV}/>
+            </div>
+    )
+}
+export default Resume;

2edc8e2aaee09292e21a4a229e079f774d429152
Melissa Guachun <1212malka@gmail.com>
2022-03-16 22:13:30 -0400

    break up about me

--- src/About.js
+++ src/About.js
@@ -6,9 +6,11 @@ function About() {
             <h4 className="about-blurb">
                 I'm a software engineer with a background in visual arts and literature. As a freelance illustrator,
                 I've always had an interest in image and text through my art. Through coding, I'm able to converge
-                my passions for language and design to positively impact others. As a sortware engineer, 
-                I strive to be the connection between the technical world of coding and the visual aesthetics of fine 
-                art to create functional yet visually stunning projects. I'm a graduate of the Flatiron School where I 
+                my passions for language and design to positively impact others.             As a sortware engineer, I strive to be the connection between the technical world of coding and the visual aesthetics of fine 
+                art to create functional yet visually stunning projects. 
+                <br/>
+                <br/>
+    I'm a graduate of the Flatiron School where I 
                 spent 10 months refining my skills as a full stack developer. I am eager to find my place on a 
                 collaborative team of talented individuals where I can continue expanding my knowledge and developing new skills.
                 When I'm not coding new projects, you can find me working on my art, digging through crates at a record store, or 

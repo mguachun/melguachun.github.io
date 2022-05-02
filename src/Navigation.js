de4666d64892ae34a9f12679cdd0f78e17778be9
Melissa Guachun <1212malka@gmail.com>
2022-05-02 14:02:06 -0400

    debug nav

--- src/Navigation.js
+++ src/Navigation.js
@@ -5,31 +5,31 @@ import './App.css';
 function Navigation() {
   return (
     <div id="nav">
-     
+
       {/* <Nav defaultActiveKey="/home" as="ul"> */}
       <div className="nav-left">
         <p className="nav-name">Melissa Guachun
           <img src={require("./images/computer-plain.jpg")} alt="logo" id="logo" />
         </p>
       </div>
-      {/* <div> */}
-{/* 
-        <Nav.Item as="li" className="nav-right">
-          <Nav.Link href="/#about">About</Nav.Link>
-        </Nav.Item>
-        <Nav.Item as="li">
-          <Nav.Link href="/#skills" className="nav-right">Skills</Nav.Link>
-        </Nav.Item>
-        <Nav.Item as="li">
-          <Nav.Link href="/#projects" className="nav-right">Projects</Nav.Link>
-        </Nav.Item>
-        <Nav.Item as="li">
-          <Nav.Link href="/#contact" className="nav-right">Contact</Nav.Link>
-        </Nav.Item>
-      </Nav> */}
+      {/* <div>
+        <Nav>
+          <Nav.Item as="li" className="nav-right">
+            <Nav.Link href="/#about">About</Nav.Link>
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Nav.Link href="/#skills" className="nav-right">Skills</Nav.Link>
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Nav.Link href="/#projects" className="nav-right">Projects</Nav.Link>
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Nav.Link href="/#contact" className="nav-right">Contact</Nav.Link>
+          </Nav.Item>
+        </Nav> */}
 
-    </div>
+      </div>
 
-  )
+      )
 }
-export default Navigation;
+    export default Navigation

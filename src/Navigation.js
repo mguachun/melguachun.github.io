e722e070646f67445f77f508c18e11268c7f241c
Melissa Guachun <1212malka@gmail.com>
2022-05-25 14:46:06 -0400

    fixing nav

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,35 +1,32 @@
 import React from "react";
-// import { Link } from "react-scroll";
-// import {Nav} from 'react-bootstrap'
+import Projects from "./Projects"
+import Nav from 'react-bootstrap/Nav';
+import { Link } from "react-scroll";
+// import { HashLink as Link } from 'react-router-hash-link';
 import './App.css';
 function Navigation() {
   return (
     <div id="nav">
-
-      {/* <Nav defaultActiveKey="/home" as="ul"> */}
       <div className="nav-left">
         <p className="nav-name">Melissa Guachun
           <img src={require("./images/computer-plain.jpg")} alt="logo" id="logo" />
         </p>
       </div>
-      {/* <div>
-        <Nav>
-          <Nav.Item as="li" className="nav-right">
-            <Nav.Link href="/#about">About</Nav.Link>
-          </Nav.Item>
-          <Nav.Item as="li">
-            <Nav.Link href="/#skills" className="nav-right">Skills</Nav.Link>
-          </Nav.Item>
-          <Nav.Item as="li">
-            <Nav.Link href="/#projects" className="nav-right">Projects</Nav.Link>
-          </Nav.Item>
-          <Nav.Item as="li">
-            <Nav.Link href="/#contact" className="nav-right">Contact</Nav.Link>
-          </Nav.Item>
-        </Nav> */}
+      <Link className="nav-button" to="about" spy={true} smooth={false}>
+          About
+        </Link>
+        <Link className="nav-button" to="skills" spy={true} smooth={false}>
+          Skills
+        </Link>
+        <Link className="nav-button" to="projects" spy={true} smooth={false}>
+          Projects
+        </Link>
+        <Link className="nav-button" to="contact" spy={true} smooth={false}>
+          Contact
+        </Link>
 
       </div>
-
+     
       )
 }
     export default Navigation

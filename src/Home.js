f61c95ec9ad78d772b9213b8fd2876a91793c7c9
Melissa Guachun <1212malka@gmail.com>
2022-05-31 15:14:14 -0400

    changed job titles

--- src/Home.js
+++ src/Home.js
@@ -1,51 +1,43 @@
 import React from "react";
 import self from "./images/profile_img.png"
-import linkedin from './images/linkedin.png'
-import github from './images/github.png'
-import dev from './images/dev.png'
+// import linkedin from './images/linkedin.png'
+// import github from './images/github.png'
+// import dev from './images/dev.png'
+import paint from './images/paint.png'
+import coding from './images/coding.png'
 
 function Home() {
-    return(
- 
-        <div className="container">
-          <div className="row justify-content-center home-container">
-            <div className="col-md-5">
-              <div className="img-wrapper">
-              <img className="profile-image"  src={self} alt="profile-img" id="profile-img"></img>
-              </div>
-              {/* <img className="profile-image"  src={self} alt="profile-img" id="profile-img"></img> */}
-              <div id="intro">
-              {/* src={require("./images/profile_img.png")} */}
-                <br></br>
-                <h1 > Hey, I'm Melissa</h1>
-                <h4>Software Engineer, Visual Artist, Creative </h4>
-                <a
-            href="https://www.linkedin.com/in/melissa-guachun/"
-            // target="_blank" 
-            className="background-none"
-          >
-        <img src={linkedin} alt="" className="contact-icon" />
-        </a>
-        <a
-            href="https://github.com/mguachun"
-            // target="_blank"
-            className="background-none"
-          >
-        <img src={github} alt="" className="contact-icon" />
-        </a>
+  return (
 
-        <a
-            href="https://dev.to/melguachun"
-            // target="_blank"
-            className="background-none"
-          >
-        <img src={dev} alt="" className="contact-icon" />
-        </a>
+    <div className="container">
+      <div className="row justify-content-center home-container">
+        <div className="col-md-5">
+          <div className="img-wrapper">
+            <img className="profile-image" src={self} alt="profile-img" id="profile-img"></img>
+          </div>
+          {/* <img className="profile-image"  src={self} alt="profile-img" id="profile-img"></img> */}
+          <div id="intro">
+            {/* src={require("./images/profile_img.png")} */}
+            <br></br>
+            <h1> Hey, I'm Melissa</h1>
+            <br></br>
+            <div className="leftAdj">
+        
+            <h3>Software Engineer </h3>
+            <img className="values" src={coding} />
             </div>
+            <div className="rightAdj">
+            <h3>Visual Artist</h3>
+            <img className="values" src={paint}></img>
+
+            </div>
+            <div>
             </div>
           </div>
         </div>
+      </div>
+    </div>
 
-    )
+  )
 }
 export default Home;
